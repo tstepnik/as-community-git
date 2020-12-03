bdbda50e083242538841bb8c922b0be12fabf44d
tstepnik <tomasz.stepnik77@gmail.com>
2020-12-03 09:51:50 +0100

    ready to code review

--- src/aura/AS_Community_ProductDetails/AS_Community_ProductDetailsController.js
+++ src/aura/AS_Community_ProductDetails/AS_Community_ProductDetailsController.js
@@ -2,6 +2,9 @@
 
     onInit: function (component, event, helper) {
         helper.loadPrice(component, event, helper);
-    }
+    },
+    addToBasket: function(component, event, helper){
+        helper.addProductToOrder(component,event,helper);
+    },
 
 })
--- src/aura/AS_Community_ProductDetails/AS_Community_ProductDetailsHelper.js
+++ src/aura/AS_Community_ProductDetails/AS_Community_ProductDetailsHelper.js
@@ -1,6 +1,6 @@
 ({
 
-    loadPrice: function (component, event, helper) {
+    loadPrice: function (component, event) {
         const action = component.get('c.getProductPrice_apex');
         action.setParams({productId: component.get('v.recordId')});
         action.setCallback(this, function (response) {
@@ -9,13 +9,48 @@
             if (status === 'SUCCESS') {
                 let apexPrice = response.getReturnValue();
                 component.set('v.price', apexPrice);
-            } else {
-                let errors = response.getError();
-                if (errors && Array.isArray(errors) && errors.length > 0) {
-                    console.error(JSON.stringify(errors[0].message));
-                }
+            } else if (status === "ERROR") {
+            this.handleErrors(component,event,response);
             }
         });
         $A.enqueueAction(action);
+    },
+
+    addProductToOrder: function (component, event) {
+        let productId = component.get('v.recordId');
+        const action = component.get('c.addProductToBasket');
+        action.setParams({productId: productId});
+        action.setCallback(this, function (response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                let responseMessage = response.getReturnValue();
+                this.handleShowToast(component, event, 'Success', 'Success', responseMessage);
+
+            } else if (state === "ERROR") {
+           this.handleErrors(component,event,response);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    handleShowToast: function (component, event, title, variant, message) {
+        component.find('notification').showToast({
+            "title": title,
+            "variant": variant,
+            "message": message
+        });
+    },
+
+    handleErrors: function (component,event,response) {
+        this.handleShowToast(component, event, 'Error', 'Error', 'Error while processing loading data');
+        let errors = response.getError();
+        if (errors) {
+            if (errors[0] && errors[0].message) {
+                console.log("Error message: " +
+                    errors[0].message);
+            }
+        } else {
+            console.log("Unknown error");
+        }
     }
 })
